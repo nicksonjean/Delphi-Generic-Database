e3250296565815d28b7d0861f3f836a728a51c7c
Nickson Jeanmerson <nickson_jeanmerson@hotmail.com>
2020-05-10 03:20:11 -0300

    Primeiro Commit da Nova Versão
    Remodelagem do Zero da Classe SQLConnection, Agora Usando RTTI e Generics;
    Conclusão dos Três Tipos de Matrizes Unidimensionais: TArray, TArrayVariant e TArrayField;
    Implementação das Strings do Record QueryBuilder com os Três Tipos;
    Criação de Classes Auxiliares para Validação e Formatação dos Tipos: String, Float e TimeDate;
    Implementação de EventDriven para Delegação de Eventos;
    Recodificação do Zero dos Métodos de Conexão com Componentes: TComboBox, TComboEdit, TListBox, TListView, TGrid e TStringGrid, com os Métodos: ToCombo, ToGrid, ToListBox e ToListView, Refeitos Sem LiveBindings;
    
    ToDo Fazer o Helper para TArrayField;
    ToDo Iniciar o Novo Tipo de Matriz Multidimensional: TArrayAssoc;

--- Project1.dpr
+++ Project1.dpr
@@ -3,21 +3,29 @@ program Project1;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  Unit1 in 'Unit1.pas' {Form1},
-  Conversion in 'Conversion.pas',
-  ArrayAssoc in 'ArrayAssoc.pas',
-  SQLConnection in 'SQLConnection.pas',
-  MultiDetailAppearanceU in 'MultiDetailAppearanceU.pas',
-  FMXObjectHelper in 'Helpers\FMXObjectHelper.pas',
-  NotifyHelper in 'Helpers\NotifyHelper.pas',
-  FormHelper in 'Helpers\FormHelper.pas',
-  AnonumousThreadPool in 'AnonumousThreadPool.pas';
+  SQLConnection in 'SQLConnection\SQLConnection.pas',
+  EventDriven in 'SQLConnection\EventDriven\EventDriven.pas',
+  Locale in 'SQLConnection\Types\Locale\Locale.pas',
+  Connector in 'SQLConnection\Connector\Connector.pas',
+  FMX.ListView.Extension in 'SQLConnection\Extensions\FMX.ListView\FMX.ListView.Extension.pas',
+  FMX.Grid.Helper in 'SQLConnection\Helpers\FMX.Grid\FMX.Grid.Helper.pas',
+  FMX.ListBox.Helper in 'SQLConnection\Helpers\FMX.ListBox\FMX.ListBox.Helper.pas',
+  FMX.ListView.Helper in 'SQLConnection\Helpers\FMX.ListView\FMX.ListView.Helper.pas',
+  FMX.StringGrid.Helper in 'SQLConnection\Helpers\FMX.StringGrid\FMX.StringGrid.Helper.pas',
+  FMX.Edit.Helper in 'SQLConnection\Helpers\FMX.Edit\FMX.Edit.Helper.pas',
+  FMX.&Object.Helper in 'SQLConnection\Helpers\FMX.Object\FMX.Object.Helper.pas',
+  Float in 'SQLConnection\Types\Float.pas',
+  &String in 'SQLConnection\Types\String.pas',
+  &Array in 'SQLConnection\Types\Array.pas',
+  TimeDate in 'SQLConnection\Types\TimeDate.pas',
+  MimeType in 'SQLConnection\Reflection\MimeType.pas',
+  Unit1 in 'Unit1.pas' {SQLConnectionTester};
 
 {$R *.res}
 
 begin
   //ReportMemoryLeaksOnShutdown := True;
   Application.Initialize;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TSQLConnectionTester, SQLConnectionTester);
   Application.Run;
 end.
